e1762f42f2e85543bf48b687b37025a7dcb3ccc5
Elon Aseneka <elonsantos63@gmail.com>
2019-08-19 12:35:45 +0300

    nav

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+    return ( 
+        <nav>
+            <ul>
+                <li>ABOUT</li>
+                <li>PORTFOLIO</li>
+                <li>CLIENTS</li>
+                <li>SERVICES</li>
+                <li>HIRE ME</li>
+            </ul>
+        </nav>
+     );
+}
+ 
+export default Navbar;
